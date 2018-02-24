2cc46b0b6ac62f0f3e8462a3278a4c5f058e1628
Nicholas Biddell <nicbiddell@gmail.com>
2018-02-24 15:39:08 -0500

    Adjust validator to use instance of Sniffer

--- csv_web_app_server/file_upload/validators.py
+++ csv_web_app_server/file_upload/validators.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ValidationError
 
 def import_csv_file_validator(input_file):
     try:
-        dialect = csv.Sniffer.sniff(input_file.read(1024))
-        input_file.seek(0,0)
+        dialect = csv.Sniffer().sniff(input_file.read(1024).decode('utf-8'))
+        input_file.seek(0)
     except csv.Error:
         raise ValidationError('Not a valid CSV file')
