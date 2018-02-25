2f17b70f6c48c41f495a831c73e47b61055e3420
Nicholas Biddell <nicbiddell@gmail.com>
2018-02-25 14:28:44 -0500

    Add column verification in validator

--- csv_web_app_server/file_upload/validators.py
+++ csv_web_app_server/file_upload/validators.py
@@ -1,9 +1,35 @@
 import csv
+from ast import literal_eval
+
 from django.core.exceptions import ValidationError
 
 def import_csv_file_validator(input_file):
     try:
-        dialect = csv.Sniffer().sniff(input_file.read(1024).decode('utf-8'))
+        stream = input_file.read().decode('utf-8')
+        hasHeaders = csv.Sniffer().has_header(stream)
+        dialect = csv.Sniffer().sniff(stream)
+        print('did we get here')
         input_file.seek(0)
     except csv.Error:
         raise ValidationError('Not a valid CSV file')
+
+    reader = csv.reader(input_file.read().decode('utf-8').splitlines(), dialect)
+
+    for row, column in enumerate(reader):
+        # Header check is in a different validator
+        if row > 0:
+            for index, value in enumerate(column):
+
+                # Don't validate the first column
+                if index > 0:
+                    if not is_valid_column_data(value):
+                        errMsg = 'Not valid column data: {} | row, column: {}, {}'.format(value,
+                                row, index + 1)
+                        raise ValidationError(errMsg)
+
+def is_valid_column_data(value):
+    value = literal_eval(value)
+
+    is_float_or_int = (isinstance(value, int) or isinstance(value, float))
+
+    return is_float_or_int and value >= 0
