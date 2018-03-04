25fc4777221019f3e8b4d8146827a90e5df4845f
Nicholas Biddell <nicbiddell@gmail.com>
2018-03-03 22:13:33 -0500

    Set parser to MultiPartParser

--- csv_web_app_server/file_upload/views.py
+++ csv_web_app_server/file_upload/views.py
@@ -1,9 +1,13 @@
 from rest_framework import viewsets
 
+from rest_framework.parsers import MultiPartParser
+
 from csv_web_app_server.file_upload.models import CsvUpload
 from csv_web_app_server.file_upload.serializers import CsvUploadSerializer
 
 class CsvUploadViewSet(viewsets.ModelViewSet):
+    parser_classes = (MultiPartParser,)
+
     queryset = CsvUpload.objects.all()
     serializer_class = CsvUploadSerializer
 
