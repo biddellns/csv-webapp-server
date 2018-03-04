92ddba628a6e615d9a9fad613f92d393379a3ca0
Nicholas Biddell <nicbiddell@gmail.com>
2018-03-03 22:22:40 -0500

    Set document required to false.
    This never gets sent as JSON so it is always null. Therefore the call
    always fails and throws a 400. Let's hope this bad boi fixes my issues

--- csv_web_app_server/file_upload/serializers.py
+++ csv_web_app_server/file_upload/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import CsvUpload
 
 class CsvUploadSerializer(serializers.ModelSerializer):
+    document = serializers.FileField(required = False)
+
     class Meta:
         model = CsvUpload
         fields = ('pk', 'document', 'upload_date')
