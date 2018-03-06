cfc42442a9b604fc62d63e20e05e595219066822
Nicholas Biddell <nicbiddell@gmail.com>
2018-03-06 00:04:07 -0500

    Convert CSV to JSON with header:data

--- csv_web_app_server/file_upload/utilities.py
+++ csv_web_app_server/file_upload/utilities.py
@@ -2,19 +2,17 @@ import csv
 import json
 
 def convert_csv_to_json(input_file):
-    #    stream = input_file.read().decode('utf-8')
-    #dialect = csv.Sniffer().sniff(stream)
-
-    #reader = csv.reader(stream.splitlines())
     reader = csv.reader(input_file.read().decode('utf-8').splitlines())
 
     rows = []
-    for row, column in enumerate(reader):
-        new_row = {}
-        for index, value in enumerate(column):
-            new_row[index] = value
-        rows.append(new_row)
+    headers = []
+    for index, row in enumerate(reader):
+        if index == 0:
+            headers = row
+        else:
+            data_row = {}
+            for index, column in enumerate(row):
+                data_row[headers[index]] = column
+            rows.append(data_row)
 
-    
-    print(json.dumps(rows))
-    return json.dumps(rows)
+    return rows
