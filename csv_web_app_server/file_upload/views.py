e45f99612b7ccae6e41af8d2cc7bda0d61158bbf
Nicholas Biddell <nicbiddell@gmail.com>
2018-03-05 20:10:50 -0500

    Override create method in viewset

--- csv_web_app_server/file_upload/views.py
+++ csv_web_app_server/file_upload/views.py
@@ -2,7 +2,7 @@ import logging
 
 from rest_framework import viewsets
 from rest_framework.views import APIView
-from rest_framework.parsers import MultiPartParser
+from rest_framework.parsers import MultiPartParser, FileUploadParser
 from rest_framework.response import Response
 
 from csv_web_app_server.file_upload.models import CsvUpload
@@ -11,9 +11,21 @@ from csv_web_app_server.file_upload.serializers import CsvUploadSerializer
 class CsvUploadViewSet(viewsets.ModelViewSet):
     queryset = CsvUpload.objects.all()
     serializer_class = CsvUploadSerializer
+    parser_classes = (MultiPartParser,)
+
+    def create(self, request):
+        try:
+            file_obj = request.FILES['upload']
+            serializer = CsvUploadSerializer(data={'document':file_obj})
+            serializer.is_valid()
+            serializer.save()
+
+            return Response(serializer.data, status=201)
+        except:
+            return Response(status=400)
 
 class CsvUploadApiView(APIView):
-    parser_classes = (MultiPartParser,)
+    parser_classes = (MultiPartParser, FileUploadParser,)
 
     def get(self, request, pk):
         try:
